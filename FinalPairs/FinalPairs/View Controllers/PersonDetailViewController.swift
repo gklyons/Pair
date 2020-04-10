210e232dc916a7b242fa13ec4bfdb4b3ca80eb23
gklyons <30358106+gklyons@users.noreply.github.com>
2020-04-10 11:11:55 -0600

    storyboard built out and outlets + actions dragged out to view controller files

---
+++ FinalPairs/FinalPairs/View Controllers/PersonDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  PersonDetailViewController.swift
+//  FinalPairs
+//
+//  Created by Garrett Lyons on 4/10/20.
+//  Copyright © 2020 Turtle. All rights reserved.
+//
+
+import UIKit
+
+class PersonDetailViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    // MARK: - IBOutlets
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: - Actions
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+
+}
