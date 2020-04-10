8c33f6188fa2dafcf19bb1032804826b1b55d337
gklyons <30358106+gklyons@users.noreply.github.com>
2020-04-10 11:39:40 -0600

    Built out delete ability in PersonListTVC

--- FinalPairs/FinalPairs/View Controllers/PersonListTableViewController.swift
+++ FinalPairs/FinalPairs/View Controllers/PersonListTableViewController.swift
@@ -52,53 +52,35 @@ class PersonListTableViewController: UITableViewController {
     
     // MARK: - Table view data source
 
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        "Group \(section + 1)"
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 0
+        return pairs?[section].count ?? 0
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "personCell", for: indexPath)
         
+        let student = pairs?[indexPath.section][indexPath.row]
+        cell.textLabel?.text = student?.name
 
         return cell
     }
 
 
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-
-    //DELETING
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+ override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            guard let person = pairs?[indexPath.section][indexPath.row]
+                else { return }
+            pairs?[indexPath.section].remove(at: indexPath.row)
+            PersonController.sharedInstance.delete(person: person)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
+    
 
     // MARK: - Helper Functions
     func randomize() {
